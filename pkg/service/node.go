9a162ac0574ffad7bd89ce988133bf096583d4e8
Roman Bednar <rbednar@redhat.com>
2021-09-04 16:29:54 +0200

    node: move volume helper functions to separate package
    node.go will inevitably grow in size when adding new node capabilities
    due to adding new helper functions. Also it is way cleaner to separate
    similar functionality to it's own dirs and packages
    
    Create a new utils dir with packages that should not live inside
    service dir and move recently added volume helpers to a separate
    package.

--- pkg/service/node.go
+++ pkg/service/node.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"google.golang.org/grpc/codes"
@@ -17,10 +16,10 @@ import (
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/ovirt/csi-driver/internal/ovirt"
+	volumemanager "github.com/ovirt/csi-driver/pkg/utils"
 	ovirtsdk "github.com/ovirt/go-ovirt"
 
 	"golang.org/x/net/context"
-	"golang.org/x/sys/unix"
 	"k8s.io/klog"
 )
 
@@ -179,7 +178,7 @@ func (n *NodeService) NodeGetVolumeStats(ctx context.Context, req *csi.NodeGetVo
 		return nil, status.Errorf(codes.Internal, "Unknown error when getting stats on %s: %v", req.VolumePath, err)
 	}
 
-	isBlock, err := IsBlockDevice(req.VolumePath)
+	isBlock, err := volumemanager.IsBlockDevice(req.VolumePath)
 
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "Failed to determine whether %s is block device: %v", req.VolumePath, err)
@@ -187,7 +186,7 @@ func (n *NodeService) NodeGetVolumeStats(ctx context.Context, req *csi.NodeGetVo
 
 	// If volume is a block device, return only size in bytes.
 	if isBlock {
-		bcap, err := getBlockSizeBytes(req.VolumePath)
+		bcap, err := volumemanager.GetBlockSizeBytes(req.VolumePath)
 		if err != nil {
 			return nil, status.Errorf(codes.Internal, "Failed to get block size on path %s: %v", req.VolumePath, err)
 		}
@@ -202,7 +201,7 @@ func (n *NodeService) NodeGetVolumeStats(ctx context.Context, req *csi.NodeGetVo
 	}
 
 	// We assume filesystem presence on volume as raw block device is ruled out and try to get fs stats
-	available, capacity, used, inodesFree, inodes, inodesUsed, err := StatFS(req.VolumePath)
+	available, capacity, used, inodesFree, inodes, inodesUsed, err := volumemanager.StatFS(req.VolumePath)
 
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to get fs info on path %s: %v", req.VolumePath, err)
@@ -402,57 +401,4 @@ func getDeviceByMountPoint(mp string) (string, error) {
 		return "", fmt.Errorf("could not parse command output: >%s<", string(out))
 	}
 	return s[1], nil
-}
-
-func StatFS(path string) (available, capacity, used, inodesFree, inodes, inodesUsed int64, err error) {
-	statfs := &unix.Statfs_t{}
-	err = unix.Statfs(path, statfs)
-	if err != nil {
-		err = fmt.Errorf("Failed to get fs info on path %s: %v", path, err)
-		return
-	}
-
-	// Available is blocks available * fragment size
-	available = int64(statfs.Bavail) * int64(statfs.Bsize)
-
-	// Capacity is total block count * fragment size
-	capacity = int64(statfs.Blocks) * int64(statfs.Bsize)
-
-	// Usage is block being used * fragment size (aka block size).
-	used = (int64(statfs.Blocks) - int64(statfs.Bfree)) * int64(statfs.Bsize)
-
-	// Get inode usage
-	inodes = int64(statfs.Files)
-	inodesFree = int64(statfs.Ffree)
-	inodesUsed = inodes - inodesFree
-
-	return
-}
-
-func IsBlockDevice(fullPath string) (bool, error) {
-	st := &unix.Stat_t{}
-	err := unix.Stat(fullPath, st)
-	if err != nil {
-		return false, err
-	}
-
-	return (st.Mode & unix.S_IFMT) == unix.S_IFBLK, nil
-}
-
-func getBlockSizeBytes(devicePath string) (int64, error) {
-	cmd := exec.Command("blockdev", "--getsize64", devicePath)
-	out, err := cmd.Output()
-
-	if err != nil {
-		return -1, fmt.Errorf("Error when getting size of block volume at path %s: output: %s, err: %v", devicePath, string(out), err)
-	}
-
-	strOut := strings.TrimSpace(string(out))
-	gotSizeBytes, err := strconv.ParseInt(strOut, 10, 64)
-
-	if err != nil {
-		return -1, fmt.Errorf("Failed to parse %s into an int size", strOut)
-	}
-
-	return gotSizeBytes, nil
 }
