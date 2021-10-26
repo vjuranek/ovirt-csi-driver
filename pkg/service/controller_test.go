ec9a0a103016898dab428c7845af1e4e24bd9f06
Vojtěch Juránek <vjuranek@redhat.com>
2021-10-26 15:21:03 +0200

    tests: move CreateVolume into a helper function
    Volume will be create in several other tests. To avoid repeating of the
    code, move it into dedicated helper function.

--- pkg/service/controller_test.go
+++ pkg/service/controller_test.go
@@ -6,30 +6,16 @@ import (
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/ovirt/csi-driver/pkg/service"
+	ovirtclient "github.com/ovirt/go-ovirt-client"
 )
 
 func TestVolumeCreation(t *testing.T) {
 	helper := getMockHelper(t)
 	controller := service.NewOvirtCSIDriver(helper.GetClient(), "test")
-	testStorageDomain, err := helper.GetClient().GetStorageDomain(helper.GetStorageDomainID())
-	if err != nil {
-		t.Fatalf("failed to fetch test storage domain")
-	}
 
-	createVolumeResponse, err := controller.CreateVolume(context.Background(), &csi.CreateVolumeRequest{
-		Name: "test",
-		CapacityRange: &csi.CapacityRange{
-			RequiredBytes: 4096,
-			LimitBytes:    4096,
-		},
-		Parameters: map[string]string{
-			"storageClass":      "ovirt-test-domain",
-			"storageDomainName": testStorageDomain.Name(),
-			"thinProvisioning":  "true",
-		},
-	})
+	createVolumeResponse, err := createTestVolume(helper, controller)
 	if err != nil {
-		t.Fatalf("failed to create volume (%v)", err)
+		t.Fatalf("failed to create test volume (%v)", err)
 	}
 	if createVolumeResponse.Volume.CapacityBytes < 4096 {
 		t.Fatalf("created volume is too small (%v)", err)
@@ -45,4 +31,29 @@ func TestVolumeCreation(t *testing.T) {
 	if disk.TotalSize() < 4096 {
 		t.Fatalf("incorrect disk size on the backend: %d", disk.TotalSize())
 	}
+}
+
+func createTestVolume(helper ovirtclient.TestHelper, controller *service.OvirtCSIDriver) (*csi.CreateVolumeResponse, error) {
+	testStorageDomain, err := helper.GetClient().GetStorageDomain(helper.GetStorageDomainID())
+	if err != nil {
+		return nil, err
+	}
+
+	createVolumeResponse, err := controller.CreateVolume(context.Background(), &csi.CreateVolumeRequest{
+		Name: "test",
+		CapacityRange: &csi.CapacityRange{
+			RequiredBytes: 4096,
+			LimitBytes:    4096,
+		},
+		Parameters: map[string]string{
+			"storageClass":      "ovirt-test-domain",
+			"storageDomainName": testStorageDomain.Name(),
+			"thinProvisioning":  "true",
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return createVolumeResponse, nil
 }
